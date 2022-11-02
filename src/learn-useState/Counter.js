f9e465c0e7c1ea84b1b5e23c10e04f000e866e73
taufiq28-bit <taufiqurrohman010@gmail.com>
2022-11-02 10:06:05 +0700

    menambahkan input type pada useState

--- src/learn-useState/Counter.js
+++ src/learn-useState/Counter.js
@@ -3,48 +3,76 @@ import logo from "../logo.svg";
 import "./Counter.css";
 
 export default function Counter(){
-    const [showImage, setShowImage] = useState(true);
+    const [showImage,setShowImage] = useState(true);
     const [count, setCount] = useState(0);
+    const[text, setText]= useState("Ini default Textnya");
+    const[fontSize, setFontSize]=useState(16)
+    const [color, setColor]= useState('black')
     const countUp = () => {
-        setCount(count + 1);
+        setCount(count +1); 
     };
+
     const countDown = () =>{
-        setCount(count - 1);
+        setCount(count-1);
     };
+
     const hideImage = () => {
         setShowImage(!showImage);
     };
-    
-    return (
-        <div className="Main">
-            <p className="Text">Counter with useState</p>
-            <p>KELOMPOK 03</p>
-            <div className="ViewImage">
-                <div className="ViewImage1">
-                    <img className="App-logo"
-                    style={{display: showImage === true ? "flex" :
-                    "none"}}
-                    src={logo}
-                    alt="logo"/>
-                </div>
-                <button className="Button" onClick={hideImage}>
-                    {showImage !== true ?"show" :"hide"}
+
+
+return (
+    <div className="Main">
+        <p className="Text">Counter with useState</p>
+        <p>KELOMPOK 03</p>
+       
+        <h3 style={{//{}ini tandanya objek
+          fontSize: fontSize+'px',
+          color:color, //ada color disini
+        }}>{text}</h3>
+
+        <input type="text" placeholder="masukkan text" 
+        onChange={(e)=>{
+          setText(e.target.value);
+        }} />
+        <div className="ViewImage">
+            <div className="ViewImage1">
+                <img className="App-logo"
+                style={{display: showImage === true ? "flex" :
+"none"}}
+                src={logo}
+                alt="logo"/>
+            </div>
+            <div>
+            <input type="range" min={1} max={100}
+             onChange={(e)=>{
+            setFontSize(e.target.value)
+            }} />
+            </div>
+            <div>
+            <input type="color" 
+            onChange={(e)=>{
+            setColor(e.target.value);
+            }}
+            />
+            </div>
+            <button className="Button" onClick={hideImage}>
+                {showImage !== true ?"show" :"hide"}
+            </button>
+        </div>
+        <p className="Text">{count}</p>
+        <div className="ViewButton">
+            <div className="ViewButton2">
+                <button className="Button" onClick={countUp}>
+                    Up
                 </button>
             </div>
-            
-            <p className="Text">{count}</p>
-            <div className="ViewButton">
-                <div className="ViewButton2">
-                    <button className="Button" onClick={countUp}>
-                        Up
-                    </button>
-                </div>
-                <div className="ViewButton1">
-                    <button className="Button" onClick={countDown}>
-                        Down
-                    </button>
-                </div>
+            <div className="ViewButton1">
+                <button className="Button" onClick={countDown}>
+                    Down
+                </button>
             </div>
         </div>
-    );
-}
+    </div>
+);
+} 
